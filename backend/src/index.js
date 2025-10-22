cb03c175f38f27dfb5e3c7ddd901f57ff41b18f4
yuna <yuna5470@naver.com>
2025-10-22 19:59:28 +0900

    A

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,12 @@
 import { Hono } from 'hono'
 import kakaoRoute from '../routes/auth/kakao.js'
 import sessionRoute from '../routes/auth/session.js'
+import naver from '../routes/auth/naver.js'
 
 const app = new Hono()
 
 app.get('/', (c) => c.json({ ok: true, name: 'RunCrew API' }))
-
+app.route('/auth/naver', naver)
 app.route('/auth/kakao', kakaoRoute)
 app.route('/me', sessionRoute)
 
